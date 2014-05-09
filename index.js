ee320a5578bf8541229a10887ec47d9993e445ee
Ben West <bewest@gmail.com>
2014-05-09 14:48:21 -0700

    WIP: start working on session/{Read|Transmit}Packet
    Start working on basic infrastructure needed to transmit packets with this
    modem.

--- index.js
+++ index.js
@@ -1,7 +1,97 @@
 
+var chainsaw = require('chainsaw')
+  , EventEmitter = require('event-emitter').EventEmitter
+;
 
 var uart = require('./lib/stick/');
 
+function session (stick) {
+  var my = {stick: stick};
+
+  stick.tap(function ( ) {
+    console.log('tapped');
+  });
+
+  function api (saw) {
+    this.open = function begin ( ) {
+      var self = this;
+      stick.open(function ( ) {
+        saw.nest(function ( ) {
+          // stick.poll_signal( );
+          self.status(function (err, results) {
+            saw.next( );
+          });
+          saw.next( );
+        })
+      });
+    }
+
+    function end ( ) {
+      stick.close( );
+      saw.next( );
+    }
+    this.end = end;
+
+    function status (cb) {
+      stick.stats(function (err, results) {
+        my.stats = results;
+        if (cb) cb(err, my.stats);
+        saw.next( );
+      });
+
+    }
+    this.status = status;
+
+    function model ( ) {
+
+      saw.next( );
+    }
+    this.model = model;
+
+    function execute (command, cb) {
+      /*
+      stick
+        .transmit(command)
+        // .sleep(command.sleep)
+        // .poll_size( )
+        .download(command)
+        .tap(function ( ) {
+          cb.call(this, command);
+          saw.next( );
+        })
+      ;
+      */
+    }
+    this.exec = execute;
+
+    this.serial = function (serial) {
+      my.serial = serial ? serial : my.serial;
+      // this.emit('serial', my.serial);
+      saw.next( );
+    }
+
+  }
+  return chainsaw(api);
+}
+function pumpcommand (opts) {
+  var self = new EventEmitter;
+  self.format = format.bind(self);
+  self.packet = packet.bind(self);
+  self.response = response.bind(self);
+  self.opts = opts;
+  return self;
+}
+
+function format ( ) {
+  var head = [ ];
+  var buffer = new Buffer( );
+}
+
+function packet ( ) {
+}
+
+function response ( ) {
+}
 
 if (!module.parent) {
   var Serial = require('serialport');
@@ -9,18 +99,11 @@ if (!module.parent) {
   serial.open(function ( ) {
     console.log('stick', serial);
     var driver = uart(serial);
-    driver.open(function (ctx) {
-      // driver.usb_stats(console.log.bind(console, 'USB STATS'));
-      console.log('opened', 'args', arguments, 'this', this);
-      // driver.interface_stats(console.log.bind(console, 'INTERFACE STATS'));
-      // driver.finish( );
-    })
-    .stats(console.log.bind(console, 'INTERFACE STATS'))
-    .signal_status(console.log.bind(console, 'signal stength'))
-    .poll_signal(console.log.bind(console, 'SIGNAL'))
-    .status(console.log.bind(console, 'STATUS'))
-    .stats(console.log.bind(console, 'INTERFACE STATS'))
-    .close( )
+    var pump = session(driver);
+    pump.open( )
+        .status(console.log.bind(console, "STATUS"))
+        .status(console.log.bind(console, "STATUS"))
+        .end( )
     ;
   });
 }
