9330159521ec9c936c3a1fcabdce1f72fd4f0421
Ben West <bewest@gmail.com>
2014-05-09 21:12:44 -0700

    WIP: barebones mechanics in place to transmit/read
    Exchanges one message with pump (assuming power ctrl is already on).

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var chainsaw = require('chainsaw')
-  , EventEmitter = require('event-emitter').EventEmitter
+  , buffers = require('buffers')
+  , EventEmitter = require('events').EventEmitter
 ;
 
 var uart = require('./lib/stick/');
@@ -42,27 +43,28 @@ function session (stick) {
     }
     this.status = status;
 
-    function model ( ) {
-
-      saw.next( );
-    }
-    this.model = model;
-
     function execute (command, cb) {
-      /*
+      console.log("BEGIN", "EXECUTE", command);
       stick
         .transmit(command)
-        // .sleep(command.sleep)
-        // .poll_size( )
         .download(command)
         .tap(function ( ) {
           cb.call(this, command);
+          console.log("REMOTE", command);
           saw.next( );
         })
       ;
-      */
     }
     this.exec = execute;
+
+    function model (cb) {
+      this.exec(ReadPumpModel(my.serial), function (err, response) {
+        console.log("FETCHED PUMP MODEL", arguments);
+        cb.apply(this, arguments);
+        saw.next( );
+      })
+    }
+    this.model = model;
 
     this.serial = function (serial) {
       my.serial = serial ? serial : my.serial;
@@ -75,22 +77,79 @@ function session (stick) {
 }
 function pumpcommand (opts) {
   var self = new EventEmitter;
-  self.format = format.bind(self);
-  self.packet = packet.bind(self);
-  self.response = response.bind(self);
+  self.request = request.bind(self);
+  self.readable = readable.bind(self);
+  self.done = done.bind(self);
+  self.append = append.bind(self);
   self.opts = opts;
+  self.data = buffers( );
+  frames.call(self);
+  sleep.call(self);
   return self;
 }
 
-function format ( ) {
-  var head = [ ];
-  var buffer = new Buffer( );
+function request ( ) {
+  var req = {
+    params: this.opts.params || [ ]
+  , op: this.opts.op
+  , magic: (this.opts.op == 93 ? 85 : 0)
+  , retries: this.opts.retries || 3
+  , pageMode: this.pageMode
+  , serial: new Buffer(this.opts.serial, 'hex')
+  };
+  return req;
 }
 
-function packet ( ) {
+function frames ( ) {
+  var stat = this.opts.frames ? this.opts.frames : { };
+  this.expected = (stat.size || 64) * (stat.count || 1);
+  this.frames = Math.ceil(this.expected / 64);
+  this.pageMode = this.opts.pageMode || Math.min(this.frames, 2);
+  return this;
 }
 
-function response ( ) {
+function sleep ( ) {
+  this.pollInterval = this.opts.poll || 100;
+  this.effectTime = this.opts.effectTime || 500;
+}
+
+function append (data) {
+  console.log("ZZZ", "APPEND DATA", data);
+  this.data.push(data);
+}
+
+function readable (radio) {
+  if (this.data.slice( ).length >= this.expected) {
+    return false;
+  }
+  if (radio.size > 14) {
+    if (this.frames < 1) {
+      return true;
+    }
+    if (this.frames >= 1 && stats.size > 64) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function done ( ) {
+  if (this.data.slice( ).length >= this.expected) {
+    return true;
+  }
+  return false;
+}
+
+function append (data) {
+  console.log('XXYYXX', 'DATA', data.toString('hex'));
+  this.data.push(data);
+}
+
+function ReadPumpModel (address) {
+  return pumpcommand({
+    serial: address
+  , op: 141
+  });
 }
 
 if (!module.parent) {
@@ -101,7 +160,9 @@ if (!module.parent) {
     var driver = uart(serial);
     var pump = session(driver);
     pump.open( )
+        .serial('208850')
         .status(console.log.bind(console, "STATUS"))
+        .model(console.log.bind("MODEL NUMBER"))
         .status(console.log.bind(console, "STATUS"))
         .end( )
     ;
