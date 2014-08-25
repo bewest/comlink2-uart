d087fefd051cc7597a43c3643367ebc7a918e7cd
Ben West <bewest@gmail.com>
2014-08-25 12:08:05 -0700

    take options

--- examples/bolus_usb.js
+++ examples/bolus_usb.js
@@ -4,6 +4,7 @@ var usb = require('../lib/usb');
 if (!module.parent) {
   var prog = process.argv.slice(1,2).pop( );
   var serial = process.argv.slice(2,3).pop( ) || process.env['SERIAL'];
+  var units = process.argv.slice(3,4).pop( ) || .5;
   if (!serial) {
     console.log('usage: ', prog, 'SERIAL'); 
     process.exit(1);
@@ -20,14 +21,18 @@ if (!module.parent) {
   var session = create(stream)
 
   var pump = session.session;
+  var bolus = {strokes: 10, units: parseFloat(units)};
   pump.open(console.log.bind(console, "OPENED"))
       .serial(serial)
       .prelude({minutes: 1})
       .ReadPumpModel(function model (res, msg) {
         console.log('MODEL', res);
         console.log("ERROR?", msg);
+        if (parseInt(res) > 522) {
+          bolus.strokes = 40;
+        }
       })
-      .Bolus({strokes: 10, units: .5}, function (err, msg) {
+      .Bolus(bolus, function (err, msg) {
         console.log("BOLUS!! err", err);
         console.log("BOLUS!! msg", msg);
       })
